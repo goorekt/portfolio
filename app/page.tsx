2f50a6e2a7a4034c1d4d2fb3166e152ea1246e11
goorekt <asadiali008@gmail.com>
2023-07-26 22:44:37 +0200

    s

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-[#dfd5a5]  ">
-      <div className="grid w-full grid-cols-3 lg:grid-cols-5 leading-normal gap-3 text-[30px]">
-        <div className=" transition-all w-full h-full bg-[#dbad6a] col-span-2 lg:col-span-4  font-mono font-bold p-3 hover:scale-[101%] text-5xl  items-center">
+      <div className="grid w-full grid-cols-3 lg:grid-cols-5 leading-normal gap-3 text-[30px] lg:gap-5">
+        <div className=" transition-all w-full h-full bg-[#dbad6a] col-span-2 lg:col-span-4 lg:text-9xl font-mono font-bold p-3 hover:scale-[101%] text-5xl  items-center">
           {" "}
           ALI ASADI
         </div>
-        <div className="bg-[#628395] w-full h-full col-span-1 text-4xl font-mono p-3 hover:scale-[101%] font-bold text-center content-between leading-normal align-center flex items-center">
+        <div className="bg-[#628395] w-full h-full col-span-1 text-4xl font-mono p-3 hover:scale-[101%] lg:text-9xl font-bold text-center content-between leading-normal lg:leading-relaxed align-center flex items-center">
           {" "}
           18 ÅR
         </div>
@@ -23,4 +23,3 @@ export default function Home() {
     </main>
   );
 }
-https://coolors.co/628395-96897b-dfd5a5-dbad6a-cf995f
