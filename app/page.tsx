b75ce1ffb3f092b1a7c776fede7e247c241732af
goorekt <asadiali008@gmail.com>
2023-07-26 15:02:44 +0200

    fgdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#dfd5a5]  ">
-    <div className='grid w-full grid-cols-5 leading-normal gap-5'>
-    <div className='bg-[#dbad6a] w-full h-full col-span-4 text-9xl font-mono p-3 hover:scale-[101%] transition-all items-center' > ALI ASADI</div>
-    <div className='bg-[#628395] w-full h-full col-span-1 text-7xl font-mono p-3 hover:scale-[101%] text-center content-between leading-normal align-center flex items-center' > 18 ÅR</div>
-    <div className='bg-[#628395] w-full h-full col-span-3 text-7xl font-mono p-3 hover:scale-[101%] text-center content-between leading-normal align-center flex items-center' >FRONTEND</div>
-    <div className='bg-[#dbad6a] w-full h-full col-span-2 text-6xl font-mono p-3 hover:scale-[101%] text-center flex items-center' > COMPUTER ENTHUSIAST</div>
-    
-
-
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-[#dfd5a5]  ">
+      <div className="grid w-full grid-cols-3 lg:grid-cols-5 leading-normal gap-3 text-[30px]">
+        <div className=" transition-all w-full h-full bg-[#dbad6a] col-span-2 lg:col-span-4  font-mono font-bold p-3 hover:scale-[101%] text-5xl  items-center">
+          {" "}
+          ALI ASADI
+        </div>
+        <div className="bg-[#628395] w-full h-full col-span-1 text-4xl font-mono p-3 hover:scale-[101%] font-bold text-center content-between leading-normal align-center flex items-center">
+          {" "}
+          18 ÅR
+        </div>
+        <div className="bg-[#628395] w-full uppercase h-full text-xl font-mono p-3 hover:scale-[101%] text-center content-between leading-normal align-center flex items-center">
+          design
+        </div>
+        <div className="bg-[#dbad6a] w-full h-full col-span-2 uppercase text-2xl font-mono p-3 hover:scale-[101%] text-center grid items-center justify-center">
+          {" "}
+          overskuelighed
+        </div>
+      </div>
     </main>
-  )
+  );
 }
